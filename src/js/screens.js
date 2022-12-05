c6e0e23d2eb4871433f83bfcdc8d8684d033d60b
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2022-12-05 19:41:21 +0100

    working on png sequence funcionality for #36

--- src/js/screens.js
+++ src/js/screens.js
@@ -41,13 +41,19 @@ window.onload = (event) => {
 let animationInfo;
 socket.on("animation-info", (info) => {
   animationInfo = info;
-  console.log("the animation info:", animationInfo);
+
   loadImages(animationInfo);
-  for (let i = 0; i <animationInfo.animations.length; i++){
-    loadedImagesLimit +=animationInfo.animations[i].long;
+  for (let i = 0; i < animationInfo.animations.length; i++) {
+    // const object = {};
+    animationInfo.animations[i].playInfo = {
+      long: { index: 0, play: false, max: animationInfo.animations[i].long },
+      short: { index: 0, play: false, max: animationInfo.animations[i].short },
+    };
+    loadedImagesLimit += animationInfo.animations[i].long;
     loadedImagesLimit += animationInfo.animations[i].short;
     console.log(loadedImagesLimit);
-}
+  }
+  console.log("the animation info:", animationInfo);
 });
 
 let imagesList = [];
@@ -162,7 +168,7 @@ const loadImage = (src, id) => {
       // showImage(img);
       $images.appendChild(img);
       totalLoadedImages++;
-      if (totalLoadedImages===loadedImagesLimit){
+      if (totalLoadedImages === loadedImagesLimit) {
         console.log("all images loaded");
       }
     };
@@ -175,36 +181,138 @@ const loadImage = (src, id) => {
 
 let imageIndex = 0;
 let previousIndex = 0;
-const $loading = document.getElementById('loading');
+const $loading = document.getElementById("loading");
 const loop = () => {
- if (totalLoadedImages===loadedImagesLimit){
+  if (totalLoadedImages === loadedImagesLimit) {
     $loading.textContent = "done loading";
-     if (imagesList.length > 0) {
-       if (imagesList[0].long.length > 0) {
-         console.log(imagesList[0].long.length);
-         showImage(imagesList[0].long[imageIndex]);
-         previousIndex = imageIndex - 1;
-         if (previousIndex < 0) {
-           previousIndex = imagesList[0].long.length - 1;
-         }
-         hideImage(imagesList[0].long[previousIndex]);
+    if (imagesList.length > 0) {
+        playInfo.forEach(playItem => {
+            // const animationImages = imagesList.find(item=>item.name === `animation-${playItem.animation}`);
+            // const info = animationInfo.animations.find(
+            //   (item) => item.name === `animation-${playItem.animation}`
+            // );
+            // console.log(animationImages, playItem);
+            const length = playItem.images.length;
+            //hide all images
+            playItem.images.forEach(img => hideImage(img));
 
-         console.log(imagesList[0].long[imageIndex], imageIndex);
-       }
-       imageIndex++;
-       if (imageIndex >= imagesList[0].long.length - 1) {
-         imageIndex = 0;
-       }
-     }
- }
- else {
-    $loading.textContent = 'loading';
- }
+            //only show current image
+            const image = playItem.images[playItem.index];
+            showImage(image);
+            playItem.index++;
+
+            if (playItem.index>=length) {
+            //if it's the long state, we repeat the animation
+            if (playItem.long) {
+                    playItem.index = 0;
+                }
+            //if it's the short animations, we remove the animation from the playInfo list once it is done
+            if (playItem.short) {
+              //remove the previous play info for this animation, if there was info about it already
+              playInfo.splice(playInfo.indexOf(playItem), 1);
+              //we let server know the animation has ended 
+              socket.emit('short-ended', playItem);
+            }
+        }   
+        })
+
+        // animationInfo.animations.forEach(animation => {
+
+        // })
+
+        // const animation = imagesList.find()
+    //   if (imagesList[0].long.length > 0) {
+    //     console.log(imagesList[0].long.length);
+    //     showImage(imagesList[0].long[imageIndex]);
+    //     previousIndex = imageIndex - 1;
+    //     if (previousIndex < 0) {
+    //       previousIndex = imagesList[0].long.length - 1;
+    //     }
+    //     hideImage(imagesList[0].long[previousIndex]);
+
+    //     console.log(imagesList[0].long[imageIndex], imageIndex);
+    //   }
+    //   imageIndex++;
+    //   if (imageIndex >= imagesList[0].long.length - 1) {
+    //     imageIndex = 0;
+    //   }
+    }
+  } else {
+    $loading.textContent = "loading";
+  }
   window.requestAnimationFrame(loop);
 };
 
 window.requestAnimationFrame(loop);
 // setInterval(loop, 1000);
+
+let playInfo = [];
+socket.on("pngs", (info) => {
+    console.log(currentScreen)
+    if (info.screen==currentScreen) {
+        console.log('start right animation');
+
+        // const previous = playInfo.find(item => item.animation === info.animation);
+        // if (previous) {
+        //   //remove the previous play info for this animation, if there was info about it already
+        //   playInfo.splice(
+        //     playInfo.indexOf(previous),
+        //     1
+        //   );
+        // }
+        // //reset the playing info
+        // const currentInfo = animationInfo.animations.find(
+        //   (item) => item.name === `animation-${info.animation}`
+        // );
+        // if (currentInfo) {
+        //     currentInfo.playInfo.long.index = 0;
+        //     currentInfo.playInfo.short.index = 0;
+        //     currentInfo.playInfo.long.play = info.long;
+        //     currentInfo.playInfo.short.play = info.short;
+        // }
+
+        //get the duration state
+        let durationState;
+        if (info.long) {
+            durationState = 'long';
+        }
+        if (info.short) {
+            durationState = 'short';
+        }
+
+        //if no state is true, we remove the animation info from the playing list and hide the images
+        if (!durationState) {
+          const previous = playInfo.find(
+            (item) => item.animation === info.animation
+          );
+          
+          if (previous) {
+            previous.images.forEach((img) => hideImage(img));
+            playInfo.splice(
+              playInfo.indexOf(previous),
+              1
+            );
+          }
+        }
+
+        else {
+          //reset index
+          info.index = 0;
+          //get right images
+          const animationImages = imagesList.find(
+            (item) => item.name === `animation-${info.animation}`
+          );
+          info.images = animationImages[durationState];
+          playInfo.push(info);
+          
+        }
+        console.log(playInfo);
+        
+    }
+    else {
+        console.log('ignore');
+    }
+});
 
 //catch server response of own chosen screen
 socket.on("screen choice", (chosenScreen) => {
