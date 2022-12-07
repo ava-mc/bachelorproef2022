98d58616b644f329ce938bc5f89ed36b7eba33d0
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2022-12-07 17:29:47 +0100

    separting screensaver logic for #52

---
+++ src/js/node-functions/screensaver.js
@@ -0,0 +1,49 @@
+import { writeToArduino, io } from "../../../index.js";
+
+let playScreenSaver = true;
+let screenSaverTimer;
+let screenSaverCounter = 0;
+const screenSaverMaxWait = 10;
+
+const showScreenSaver = () => {
+  writeToArduino("s");
+
+  //let browser know it has started
+  io.emit("screensaverStart");
+};
+
+export const startScreensaverTimer = () => {
+  //to be safe, we stop the timer before we start it, so only 1 timer can run at the same time.
+  stopScreenSaverTimer();
+
+  playScreenSaver = true;
+
+  screenSaverTimer = setInterval(() => {
+    screenSaverCounter++;
+    console.log(screenSaverCounter);
+    if (screenSaverCounter > screenSaverMaxWait) {
+      //let arduino know to start screensaver effect
+      if (playScreenSaver) {
+        console.log("start screensaver");
+        showScreenSaver();
+        playScreenSaver = false;
+      }
+    }
+  }, 1000);
+};
+
+export const stopScreenSaverTimer = () => {
+  console.log("stop screensaver");
+  //let arduino now to stop
+  writeToArduino("t");
+
+  //stop timer
+  clearInterval(screenSaverTimer);
+
+  //reset timer
+  screenSaverCounter = 0;
+  playScreenSaver = false;
+
+  //let browser know it has stopped
+  io.emit("screensaverStop");
+};
