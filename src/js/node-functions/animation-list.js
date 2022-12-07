8585d724e7ab11c8b526663a0bbb831be641ee1e
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2022-12-07 17:45:32 +0100

    separating global constant variables in different files for #52

---
+++ src/js/node-functions/animation-list.js
@@ -0,0 +1,41 @@
+export const animationList = [
+  {
+    startMessage: "1",
+    longMessage: "a",
+    endMessage: "b",
+    counter: 0,
+    timer: null,
+    ended: false,
+    arduinoEnd: "animation-end",
+    animationInfo: {
+      screen: 2,
+      animation: 2,
+    },
+  },
+  {
+    startMessage: "2",
+    longMessage: "c",
+    endMessage: "d",
+    counter: 0,
+    timer: null,
+    ended: false,
+    arduinoEnd: "animation2-end",
+    animationInfo: {
+      screen: 3,
+      animation: 4,
+    },
+  },
+  {
+    startMessage: "3",
+    longMessage: "e",
+    endMessage: "f",
+    counter: 0,
+    timer: null,
+    ended: false,
+    arduinoEnd: "animation3-end",
+    animationInfo: {
+      screen: 3,
+      animation: 3,
+    },
+  },
+];
