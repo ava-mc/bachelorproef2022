6aab22b02fe44a4cdd054716d3cedd78a880c9ec
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2022-12-07 17:36:20 +0100

    moving function to library file for #52

--- src/js/lib.js
+++ src/js/lib.js
@@ -3,3 +3,7 @@ export const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//function to find the key of a certain value of an object
+export const getObjKey = (obj, value) => {
+  return Object.keys(obj).find((key) => obj[key] === value);
+};
