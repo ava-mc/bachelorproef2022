9bf05268a1dff55a2fc1d208e2d9f692d704741e
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2022-12-09 18:27:47 +0100

    working on changing logic to include different versions of animations for #54

--- src/js/lib.js
+++ src/js/lib.js
@@ -7,3 +7,6 @@ export const getRandomInt = (min, max) => {
 export const getObjKey = (obj, value) => {
   return Object.keys(obj).find((key) => obj[key] === value);
 };
+
+export const amountOfScreens = 3;
+export const amountOfVersions = 2;
