7d0029bb27c1ac26525a3aa631d033125b7cb79c
Ava <ava@MacBook-Pro-2.local>
2022-12-19 15:56:19 +0100

    fixing order mistake of screensaver text for screens for #51

--- src/js/node-functions/midi-info.js
+++ src/js/node-functions/midi-info.js
@@ -1,10 +1,10 @@
 //Big keyboard
-// const midiType = [144];
-// const endSignal = 0;
+const midiType = [144];
+const endSignal = 0;
 
 //Small keyboard
-const midiType = [128, 144];
-const endSignal = 127;
+// const midiType = [128, 144];
+// const endSignal = 127;
 
 const endSignalType = midiType[0];
 
